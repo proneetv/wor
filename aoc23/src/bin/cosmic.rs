2e68803cc75eb77fa65a94205dbe0fc13bd5a742
Proneet Verma <proneet@amazon.com>
2023-12-12 00:31:51 +0000

    11b

--- aoc23/src/bin/cosmic.rs
+++ aoc23/src/bin/cosmic.rs
@@ -16,41 +16,32 @@ fn main() -> Result<(), std::io::Error> {
         }
     }
 
-    let mut row = 0;
-    while row < map.len() {
-        if (0..map[0].len()).all(|c| map[row][c] == '.') {
-            row += 1;
-            let mut newrow = Vec::new();
-            let mut i = 0;
-            while i < map[0].len() {
-                newrow.push('.');
-                i += 1;
-            }
+    let mut rowdist = Vec::new();
+    let mut coldist = Vec::new();
 
-            map.insert(row, newrow);
+    let mut r = 0;
+    for row in 0..map.len() {
+        rowdist.push(r as i64);
+        if (0..map[0].len()).all(|c| map[row][c] == '.') {
+            r += 1000000 - 1;
         }
-        row += 1;
+        r += 1;
     }
 
-    let mut col = 0;
-    while col < map[0].len() {
+    let mut c = 0;
+    for col in 0..map[0].len() {
+        coldist.push(c as i64);
         if (0..map.len()).all(|r| map[r][col] == '.') {
-            col += 1;
-
-            let mut i = 0;
-            while i < map.len() {
-                map[i].insert(col, '.');
-                i += 1;
-            }
+            c += 1000000 - 1;
         }
-        col += 1;
+        c += 1;
     }
 
     let mut galaxy = Vec::new();
     for row in 0..map.len() {
         for col in 0..map[0].len() {
             if map[row][col] == '#' {
-                galaxy.push((row as i64, col as i64));
+                galaxy.push((row, col));
             }
         }
     }
@@ -58,10 +49,8 @@ fn main() -> Result<(), std::io::Error> {
     let mut sum = 0;
     for from in &galaxy {
         for to in &galaxy {
-            print!("f{:?}", from);
-            print!("t{:?}", to);
-            println!();
-            sum += (from.0 - to.0).abs() + (from.1 - to.1).abs();
+            sum +=
+                (rowdist[from.0] - rowdist[to.0]).abs() + (coldist[from.1] - coldist[to.1]).abs();
         }
     }
 
